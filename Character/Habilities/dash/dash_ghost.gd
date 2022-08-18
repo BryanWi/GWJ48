19f183a93b4d78909f32716f636631e36417f875
BryanWi <34015543+BryanWi@users.noreply.github.com>
2022-08-18 18:21:58 -0500

    Se arregló el Tween del efecto del dash
    Antes mandaba un error pero al cambiar la propiedad de "modulate.a" a "modulate:a" ya funcionó como debe

--- Character/Habilities/dash/dash_ghost.gd
+++ Character/Habilities/dash/dash_ghost.gd
@@ -4,9 +4,10 @@ extends Sprite2D
 
 
 func _ready():
+	#var tween:Tween = get_tree().create_tween()
 	tween.set_trans(Tween.TRANS_CUBIC)
 	#tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(self,"modulate.a",0.0,2.0)
+	tween.tween_property(self,"modulate:a",0.0,2.0)
 	
 
 
